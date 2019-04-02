f859641fe715165379eaa72cf7e3bb3fc69d927a
Florian Kraus <fkraus@hm.edu>
2019-04-02 20:25:48 +0200

    Extend Bag class and add Factory class

--- aizul/gameobjects.py
+++ aizul/gameobjects.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import random
 
 class Color(Enum):
     BLUE = 1
@@ -9,15 +10,59 @@ class Color(Enum):
 
 
 class Bag:
-    pass
+    def __init__(self):
+        self.max_tiles = 100
+        self.tiles = []
+        self.used_tiles = []
+
+        for color in Color:
+            for _ in range(20):
+                self.tiles.append(Tile(color))
+        random.shuffle(self.tiles)
+
+    def print_remaining(self):
+        '''Prints color of remaining tiles.'''
+        for tile in self.tiles:
+            print(tile)
+
+    def draw_tiles(self, number=4):
+        tiles = []
+        for _ in range(number):
+            if len(self.tiles) == 0:
+                self.refill()
+            tiles.append(self.tiles.pop())
+        return tiles
+
+    def refill(self):
+        if not self.used_tiles:
+            raise IndexError("No used tiles!")
+        else:
+            self.tiles.extend(self.used_tiles)
+            random.shuffle(self.tiles)
+
+    def return_tiles(self, tiles):
+        '''
+        Return tiles to the used tiles list.
+
+        Keyword arguments:
+        tiles -- list of tiles to return
+        '''
+        for tile in tiles:
+            if type(tile) is not Tile:
+                raise TypeError("Only Tile type allowed!")
+        self.used_tiles.extend(tiles)
 
 class Tile:
     def __init__(self, color: Color):
         if(isinstance(color, Color)):
             self.color = color
         else:
-            raise TypeError("Tile color has to be of type Color")
+            raise TypeError('Tile color has to be of type Color')
+
+    def __repr__(self):
+        return self.color.name
 
 
-class TileCollection:
-    pass
+class Factory:
+    def __init__(self):
+        self.tiles = []
--- tests/test_gameobjects.py
+++ tests/test_gameobjects.py
@@ -13,9 +13,28 @@ class TestTileClass(unittest.TestCase):
         with self.assertRaises(TypeError):
             go.Tile("this is not a color")
 
+    def test_tile_string_representation(self):
+        tile = go.Tile(go.Color.BLUE)
+        self.assertTrue(str(tile) == "BLUE")
+
 class TestColorEnum(unittest.TestCase):
     def test_number_of_colors(self):
         self.assertEqual(len(go.Color),5,"Number of Colors is not 5")
+
+class TestBagClass(unittest.TestCase):
+    def test_refill_error_with_no_unused_tiles(self):
+        bag = go.Bag()
+        with self.assertRaises(IndexError):
+            bag.refill()
+
+    def test_draw(self):
+        bag = go.Bag()
+        tiles = bag.draw_tiles(5)
+        self.assertEqual(len(tiles), 5)
+        self.assertIsInstance(tiles[0], go.Tile)
+
+        with self.assertRaises(IndexError):
+            tiles = bag.draw_tiles(101)
 
 if __name__ == '__main__':
     unittest.main()
